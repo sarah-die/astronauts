d151a22c0249282a1193d563f98d6c9fbdde48eb
sarah-die <saridi@alice.de>
2023-08-17 13:49:17 +0200

    Recoil
    Basic Setup added (recoil provider and possible states)

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RecoilRoot } from 'recoil';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </QueryClientProvider>,
 );
---
+++ src/recoilState/atom.ts
@@ -0,0 +1,20 @@
+// do not mix with react-query
+// react-query: saves states that are fetched from api and to mutate those
+// recoil: saves states that are important on client side e.g. selections / dark mode / authentication
+
+import { atom } from 'recoil';
+
+export const selectedAstronaut = atom({
+  key: 'selectedAstronaut',
+  default: '' as string,
+});
+
+export const astronautFilter = atom({
+  key: 'astronautFilter',
+  default: 'all' as string,
+});
+
+export const colorScheme = atom({
+  key: 'colorScheme',
+  default: 'light' as string,
+});
