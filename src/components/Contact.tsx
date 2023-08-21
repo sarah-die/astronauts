731976c6c606db1578191d33ea92b4b490bde371
sarah-die <saridi@alice.de>
2023-08-21 10:28:10 +0200

    refactor: changed style

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,6 +5,12 @@ export const Contact = () => {
   return (
     <Row justify="space-evenly" align="middle">
       <Col span={8}>
+        <Image
+          width={250}
+          src="https://picsum.photos/id/998/200/300?grayscale"
+        />
+      </Col>
+      <Col span={8}>
         <Title level={3}>Contact</Title>
         <Divider orientation="left">GitHub</Divider>
         <Text>Lorem ipsum dolor sit.</Text>
@@ -12,12 +18,6 @@ export const Contact = () => {
         <Text>Lorem ipsum dolor sit.</Text>
         <Divider orientation="left">E-Mail</Divider>
         <Text>Lorem ipsum dolor sit.</Text>
-      </Col>
-      <Col span={8}>
-        <Image
-          width={250}
-          src="https://picsum.photos/id/998/200/300?grayscale"
-        />
       </Col>
     </Row>
   );
