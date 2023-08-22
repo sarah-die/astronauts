c63e41c41c38dc8da7cf3c94043657bd8ea73ab9
sarah-die <saridi@alice.de>
2023-08-22 16:28:13 +0200

    feat: modal content

--- src/components/AgencyModal.tsx
+++ src/components/AgencyModal.tsx
@@ -1,11 +1,10 @@
-// import { getSearch } from '../services/agencies';
-
-import { Modal } from 'antd';
+import { Image, Modal, Typography } from 'antd';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { modalOpenState, searchParamAgencyState } from '../recoilState/atom';
 import { useQuery } from '@tanstack/react-query';
 import { getById } from '../services/agencies';
 import { AgencyDetail } from '../types';
+const { Title } = Typography;
 
 export const AgencyModal = () => {
   const [modalOpen, setModalOpen] = useRecoilState(modalOpenState);
@@ -17,18 +16,25 @@ export const AgencyModal = () => {
   });
 
   const agency: AgencyDetail | undefined = queryAgencies.data;
-  console.log('agency inside Modal', agency);
+  // console.log('agency inside Modal', agency);
 
   return (
     <>
       <Modal
-        title={searchParamAgency}
+        title={agency?.abbrev}
         centered
         open={modalOpen}
         onCancel={() => setModalOpen(false)}
         footer={null}
       >
-        {searchParamAgency}
+        {queryAgencies.status === 'loading' ? (
+          <>is loading...</>
+        ) : (
+          <>
+            <Title level={4}>{agency?.name}</Title>
+            <Image width={200} src={agency?.logo_url} />
+          </>
+        )}
       </Modal>
     </>
   );
